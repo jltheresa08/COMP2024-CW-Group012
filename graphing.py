6cdd7fe9fd642478a101e05330bc22595c93cf06
jltheresa08 <65333063+jltheresa08@users.noreply.github.com>
2023-04-22 00:54:51 +0800

    python file for graphing SA and TS

---
+++ graphing.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def plot_cities(filename):
+    # Read the city data from the file
+    with open(filename, 'r') as f:
+        cities = [line.strip().split() for line in f]
+
+    # Extract the coordinates of each city
+    x_coords = [float(city[1]) for city in cities[:-1]]
+    y_coords = [float(city[2]) for city in cities[:-1]]
+
+    # Title of graph
+    title = cities[-1][0]
+
+    # Create a plot of the cities
+    plt.plot(x_coords, y_coords, 'o', color='red')
+
+    # Connect each city to its neighbor
+    for i in range(len(cities)-1):
+        start = i
+        end = (i + 1) % len(cities[:-1])
+        plt.plot([x_coords[start], x_coords[end]], [y_coords[start], y_coords[end]], '-', color='black')
+
+    # Set the title of the plot
+    plt.title(title)
+
+    # Display the plot
+    plt.show()
+
+plot_cities('output.txt')
